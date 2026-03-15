df089190d0d53a368947912b4c7c908a290f0a12
flashychen <flashychen@sina.com>
2026-03-15 17:30:44 +0800

    Add daed source to feeds.conf.default

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 添加 daed 源码源
+echo 'src-git daed https://github.com' >> feeds.conf.default
